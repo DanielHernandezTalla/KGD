fcba8c2dbe3a6587fdac4103eb3ab2e9d189f6b4
DanielHernandezTalla <62040086+DanielHernandezTalla@users.noreply.github.com>
2024-09-07 07:00:09 -0700

    fix(contabilidad): quitando relaciones de pantalla inicial de contabilidad

--- src/app/contabilidad/page.tsx
+++ src/app/contabilidad/page.tsx
@@ -15,7 +15,7 @@ const Articulos = () => {
         <div className='grid grid-cols-buttonData gap-6 rounded-xl border-2 border-gray-200 bg-white p-4 md:p-6'>
           <ButtonData icon='faShop' text='Centro de costo' href='/contabilidad/centroDeCosto' />
           <ButtonData icon='faWarehouse' text='Almacenes' href='/contabilidad/almacenes' />
-          <ButtonData icon='tableList' text='Recepciones' href='/articulos/unidadesDeMedida' />
+          {/* <ButtonData icon='tableList' text='Recepciones' href='/articulos/unidadesDeMedida' /> */}
           {/* <ButtonData icon='faRightLeft' text='Tipo transacciones' href='/articulos/tipoTransacciones' />
           <ButtonData icon='faScrewdriverWrench' text='Artículos' href='/articulos/articulos' />
           <ButtonData icon='faShuffle' text='Conversiones' href='/articulos/conversiones' /> */}

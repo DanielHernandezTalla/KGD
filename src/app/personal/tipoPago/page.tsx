b8224391c4a78658c74d63703846bb3f74222a03
Daniel Hernandez <hernandezdany41@gmail.com>
2024-12-13 14:49:04 -0700

    feat(tipoPago) agregando autorizacion en tipo de pago

--- src/app/personal/tipoPago/page.tsx
+++ src/app/personal/tipoPago/page.tsx
@@ -4,17 +4,32 @@ import { TABLECOLUMN } from '@/interface/types';
 import { DataViewer } from '@/components/organisms';
 import { useRequest } from '@/hooks/useRequest';
 import { Pager, Search } from '@/components/molecules';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBullet } from '@/components/atoms';
 import { IDataResponse } from '@/interface/request';
+import { handrePermisos } from '@/utils/handlePermisos';
+import LayoutPermiso from '@/components/molecules/Permiso/Permiso';
 
 export default function TipoPago({ searchParams }: { searchParams: { page: number } }) {
+  const rutasToCheck: string[] = [
+    'personal.tipopago.index',
+    'personal.tipopago.store',
+    'personal.tipopago.show'
+  ];
+
+  const [checked, setChecked] = useState([] as any);
   const [valueSearch, setValueSearch] = useState({});
   const { data, isError, isLoading }: IDataResponse<any> = useRequest('tipopago', {
     pagina: searchParams?.page || 1,
     cantidadRegistrosPorPagina: 10,
     ...valueSearch
   });
+
+  // Consultar permisos y poner nombre a la pagina
+  useEffect(() => {
+    document.title = 'Tipo pago KGD';
+    handrePermisos(rutasToCheck, setChecked);
+  }, []);
 
   const tableHeaders: TABLECOLUMN[] = [
     {
@@ -45,25 +60,28 @@ export default function TipoPago({ searchParams }: { searchParams: { page: numbe
 
   return (
     <MainLayout>
-      <Pager
-        pageSize={10}
-        currentPage={Number(searchParams?.page) || 1}
-        totalCount={10 * data?.maximoPaginas}
-      >
-        <>
-          <Search getValue={setValueSearch} showBtnSearch showIcon />
-          <DataViewer
-            isLoading={isLoading}
-            isError={isError}
-            title='Tipo de pago'
-            idColumn='iD_TIPO_PAGO'
-            createHref='personal/tipoPago'
-            singleHref='personal/tipoPago'
-            cols={tableHeaders}
-            data={data?.listado}
-          />
-        </>
-      </Pager>
+      <LayoutPermiso checked={checked} name='personal.tipopago.index'>
+        <Pager
+          pageSize={10}
+          currentPage={Number(searchParams?.page) || 1}
+          totalCount={10 * data?.maximoPaginas}
+        >
+          <>
+            <Search getValue={setValueSearch} showBtnSearch showIcon />
+            <DataViewer
+              isLoading={isLoading}
+              isError={isError}
+              title='Tipo de pago'
+              idColumn='iD_TIPO_PAGO'
+              nuevo={checked['personal.tipopago.store']}
+              createHref='personal/tipoPago'
+              singleHref={checked['personal.tipopago.show'] && 'personal/tipoPago'}
+              cols={tableHeaders}
+              data={data?.listado}
+            />
+          </>
+        </Pager>
+      </LayoutPermiso>
     </MainLayout>
   );
 }

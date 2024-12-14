3de486a4f86b82b256517fe22ee4215e94bf139f
DanielHernandezTalla <62040086+DanielHernandezTalla@users.noreply.github.com>
2024-12-13 21:50:43 -0700

    feat(auth) agregando autorizacion a la pantalla de auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,27 +1,51 @@
-import type { Metadata } from 'next';
+'use client';
 import Layout from '@/components/layouts/MainLayout';
 import { ButtonData } from '@/components/atoms';
 import { TitlePage } from '@/components/molecules';
-
-export const metadata: Metadata = {
-  title: 'Autenticación KGD'
-};
+import { handrePermisos } from '@/utils/handlePermisos';
+import { useEffect, useState } from 'react';
+import LayoutPermiso from '@/components/molecules/Permiso/Permiso';
 
 const Auth = () => {
+  const rutasToCheck: string[] = [
+    'auth.auth.index',
+    'auth.usuarios.index',
+    'auth.rolespermisos.index',
+    'auth.roles.index'
+  ];
+
+  const [checked, setChecked] = useState([] as any);
+
+  // Consultar permisos y poner nombre a la pagina
+  useEffect(() => {
+    document.title = 'Autenticación KGD';
+    handrePermisos(rutasToCheck, setChecked);
+  }, []);
+
   return (
     <Layout>
-      <div className='flex flex-col gap-5 md:gap-10'>
-        <TitlePage title='Autenticación' />
-        <div className='grid grid-cols-buttonData gap-6 rounded-xl border-2 border-gray-200 bg-white p-4 md:p-6'>
-          <ButtonData icon='userGroup' text='Usuarios' href='/auth/usuarios' />
-          <ButtonData icon='faUserShield' text='Roles y permisos' href='/auth/rolesYPermisos' />
-          <ButtonData icon='settings' text='Roles' href='/auth/roles' />
-          {/* <ButtonData icon='tableList' text='Categoría permisos' href='/auth/tipoPermisos' />
+      <LayoutPermiso checked={checked} name='auth.auth.index'>
+        <div className='flex flex-col gap-5 md:gap-10'>
+          <TitlePage title='Autenticación' />
+          <div className='grid grid-cols-buttonData gap-6 rounded-xl border-2 border-gray-200 bg-white p-4 md:p-6'>
+            {checked['auth.usuarios.index'] && (
+              <ButtonData icon='userGroup' text='Usuarios' href='/auth/usuarios' />
+            )}
+
+            {checked['auth.rolespermisos.index'] && (
+              <ButtonData icon='faUserShield' text='Roles y permisos' href='/auth/rolesYPermisos' />
+            )}
+
+            {checked['auth.roles.index'] && (
+              <ButtonData icon='settings' text='Roles' href='/auth/roles' />
+            )}
+            {/* <ButtonData icon='tableList' text='Categoría permisos' href='/auth/tipoPermisos' />
           <ButtonData icon='faFileShield' text='Permisos' href='/auth/permisos' />
           <ButtonData icon='faArrowsToDot' text='Eventos' href='/auth/permisos' /> */}
-          {/* <ButtonData icon='faClipboardList' text='Tipo eventos' href='/auth/tipoEventos' /> */}
+            {/* <ButtonData icon='faClipboardList' text='Tipo eventos' href='/auth/tipoEventos' /> */}
+          </div>
         </div>
-      </div>
+      </LayoutPermiso>
     </Layout>
   );
 };

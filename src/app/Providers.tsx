b0773ae1032443bc81d2be06c2a407f86bf8fca2
Daniel Hernandez <hernandezdany41@gmail.com>
2025-01-17 13:06:56 -0700

    feat(nav) agregando validacion mediante un hook para que solo aparezcan los menus correspondientes

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,13 +2,16 @@
 import { ToastProvider } from "@/hooks/toast";
 import { AuthProvider } from "@/hooks/AuthContext";
 import { SessionProvider } from "next-auth/react";
+import { PermisosProvider } from "@/hooks/PermisosContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ToastProvider>
             <SessionProvider>
                 <AuthProvider>
-                    {children}
+                    <PermisosProvider>
+                        {children}
+                    </PermisosProvider>
                 </AuthProvider>
             </SessionProvider>
         </ToastProvider>
--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -1,40 +1,34 @@
-// import { useContext } from "react";
 import { NextLink } from '@/components/atoms/';
-// import { AuthContext } from '@/context/auth/AuthContext';
-// import { TipoPersonal } from '@/utils/types';
+import { usePermisos } from '@/hooks/PermisosContext';
 
 const Nav = () => {
-  // const { user } = useContext(AuthContext);
+  
+  const { checked } = usePermisos(); // Obtener permisos desde el contexto
 
   return (
     <nav className='flex h-full w-full flex-col gap-2'>
       <NextLink href='/' text='Inicio' icon='home' />
-      {/*<NextLink href='/pacientes' text='Pacientes' icon='paciente' /> */}
-      {/* <NextLink href="/medico" text="Médico" icon="doctor" /> */}
-      {/* {(user?.tipo === TipoPersonal.medico || user?.tipo === TipoPersonal.administrador) && (
-      )} */}
-      {/* <NextLink href="/terapeuta" text="Terapeuta" icon="servicios" /> */}
-      {/* {(user?.tipo === TipoPersonal.terapeuta || user?.tipo === TipoPersonal.administrador) && (
-      )} */}
-      {/* <NextLink href='/honorarios' text='Honorarios' icon='aseguradora' /> */}
-      {/* {(user?.tipo === TipoPersonal.terapeuta || user?.tipo === TipoPersonal.administrador) && (
-        )} */}
-      <NextLink href='/movimientos' text='Movimientos' icon='faCity' />
-      <NextLink href='/empresa' text='Empresa' icon='hotel' />
-      <NextLink href='/personal' text='Personal' icon='personal' />
-      <NextLink href='/articulos' text='Articulos' icon='tableList' />
-      <NextLink href='/contabilidad' text='Contabilidad' icon='billete' />
-      <NextLink href='/auth' text='Auth' icon='faUserShield' />
-      {/* <NextLink href='/multiples' text='Multiples' icon='settings' /> */}
-      {/* {(user?.tipo === TipoPersonal.administrador || user?.tipo === TipoPersonal.secretaria) && (
-      )} */}
-      {/* <NextLink href='/arancel' text='Arancel' icon='coins' />
-      <NextLink href='/reportes' text='Reportes' icon='reports' /> */}
-      {/* <NextLink href='/dashboard' text='Dashboard' icon='dashboard' /> */}
-      {/* {(user?.tipo === TipoPersonal.administrador || user?.tipo === TipoPersonal.secretaria) && (
-      )} */}
-      {/* <NextLink href='/usuarios' text='Usuarios' icon='userGroup' />
-      <NextLink href='/perfil' text='Perfil' icon='grupos' /> */}
+
+      {/* {checked['movimientos.recepcion.index'] && <NextLink href='/' text='Inicio' icon='home' />} */}
+
+      {checked['movimientos.movimientos.index'] && (
+        <NextLink href='/movimientos' text='Movimientos' icon='faCity' />
+      )}
+      {checked['empresa.empresa.index'] && (
+        <NextLink href='/empresa' text='Empresa' icon='hotel' />
+      )}
+      {checked['personal.personal.index'] && (
+        <NextLink href='/personal' text='Personal' icon='personal' />
+      )}
+      {checked['articulos.articulo.index'] && (
+        <NextLink href='/articulos' text='Articulos' icon='tableList' />
+      )}
+      {checked['contabilidad.contabilidad.index'] && (
+        <NextLink href='/contabilidad' text='Contabilidad' icon='billete' />
+      )}
+      {checked['auth.auth.index'] && (
+        <NextLink href='/auth' text='Auth' icon='faUserShield' />
+      )}
     </nav>
   );
 };
